94f818f4487524e39a1ac62541d3fb539925bf28
Maria Grimaldi <maria.grimaldi@edunext.co>
2021-08-13 11:34:33 -0400

    feat: first version of learning subdomain data

--- openedx_filters/learning/data.py
+++ openedx_filters/learning/data.py
@@ -7,3 +7,56 @@ conventions specified in docs/decisions/0002-events-naming-and-versioning.rst
 They also must comply with the payload definition specified in
 docs/decisions/0003-events-payload.rst
 """
+
+"""
+Data attributes for events within the architecture subdomain `learning`.
+These attributes follow the form of attr objects specified in OEP-49 data
+pattern.
+"""
+from datetime import datetime
+
+import attr
+
+@attr.s(frozen=True)
+class UserNonPersonalData:
+    """
+    Attributes defined for Open edX user object based on non-PII data.
+
+    Arguments:
+        id (int): unique identifier for the Django User object.
+        is_active (bool): indicates whether the user is active.
+    """
+
+    id = attr.ib(type=int)
+    is_active = attr.ib(type=bool)
+
+
+@attr.s(frozen=True)
+class UserPersonalData:
+    """
+    Attributes defined for Open edX user object based on PII data.
+
+    Arguments:
+        username (str): username associated with the Open edX user.
+        email (str): email associated with the Open edX user.
+        name (str): email associated with the Open edX user's profile.
+    """
+
+    username = attr.ib(type=str)
+    email = attr.ib(type=str)
+    name = attr.ib(type=str, factory=str)
+
+
+@attr.s(frozen=True)
+class UserData(UserNonPersonalData):
+    """
+    Attributes defined for Open edX user object.
+
+    This class extends UserNonPersonalData to include PII data completing the
+    user object.
+
+    Arguments:
+        pii (UserPersonalData): user's Personal Identifiable Information.
+    """
+
+    pii = attr.ib(type=UserPersonalData)
