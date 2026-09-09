1ee061121b10786a5655c7e8738f7d9a1c1fb646
irfanuddinahmad <34648393+irfanuddinahmad@users.noreply.github.com>
2026-09-09 14:07:40 +0500

    fix: remove unnecessary try/except around __version__ (not present pre-migration)

--- src/openedx_filters/__init__.py
+++ src/openedx_filters/__init__.py
@@ -3,14 +3,11 @@ Filters of the Open edX platform.
 """
 import sys
 import warnings
-from importlib.metadata import PackageNotFoundError, version
+from importlib.metadata import version
 
 from openedx_filters.filters import *
 
-try:
-    __version__ = version("openedx-filters")
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "0.0.0"
+__version__ = version("openedx-filters")
 
 if sys.version_info < (3, 12):  # pragma: no cover
     warnings.warn(
